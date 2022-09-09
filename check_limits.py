d24237ac70e215e07f11f56e77687941929ed270
nidhiv98 <72518009+nidhiv98@users.noreply.github.com>
2022-09-09 12:48:44 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,5 +1,5 @@
 
-def battery_is_ok(temperature, soc, charge_rate):
+def battery_is_ok(temperature, soc, charge_rate,message):
   
     t1=check_temp(temperature)
     s1=check_soc(soc)
@@ -11,17 +11,17 @@ def battery_is_ok(temperature, soc, charge_rate):
 def aggregate_bat(t,s,c):
    return t and s and c:
  
-def check_temp(t):
+def check_temp(temperature):
  if temperature < 0 or temperature > 45:
     return False
    return True
   
-def check_soc(s):
+def check_soc(soc):
   if  soc < 20 or soc > 80:
     return False
    return True
   
-def check_charge(c):
+def check_charge(charge_rate):
    if   charge_rate > 0.8:
     return False
    return True
@@ -32,5 +32,5 @@ def print_toConsole(msg):
   
 
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+  assert(battery_is_ok(25, 70, 0.7,'Battery is ok') is True)
+  assert(battery_is_ok(50, 85, 0,'Battery is not ok') is False)
